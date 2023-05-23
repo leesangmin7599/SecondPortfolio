420291ac913b80bf9ce3e7ed23b3afd54a680c33
Leesangmin7599 <Leesangmin7599@naver.com>
2023-05-23 22:56:54 +0900

    두번째포폴 수정

---
+++ Scripts/tdt.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tdt : MonoBehaviour
+{
+    Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
